318826351412a00e7510276ef98d166f7e9fae88
alseo0110 <alseo0110@naver.com>
2024-09-19 10:25:20 +0900

    v0.9 BP2

--- week04/week04_web01.py
+++ week04/week04_web01.py
@@ -5,14 +5,11 @@ api = 'https://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp'
 station_id = input('지역 코드 : ')  # 전국 108, 수도권 109, 강원 105, 제주 184 ...
 values = {'stnld' : station_id}
 
-url = api + '?' + values
+url = api + '?' + urllib.parse.urlencode(values)
 
 print(url)
 
 """
-Traceback (most recent call last):
-  File "C:\Users\Admin\Desktop\uni\BigData2\week04\week04_web01.py", line 8, in <module>
-    url = api + '?' + values
-          ~~~~~~~~~~^~~~~~~~
-TypeError: can only concatenate str (not "dict") to str
+지역 코드 : 184
+https://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp?stnld=184
 """
