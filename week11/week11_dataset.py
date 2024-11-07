4338a3968c36a2cb50a75e03056f817a641ce54a
alseo0110 <alseo0110@naver.com>
2024-11-07 10:51:46 +0900

    v2.8 BP2

--- week11/week11_dataset.py
+++ week11/week11_dataset.py
@@ -2,18 +2,17 @@ import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# 성별에 따른 생존율 계산
 titanic = sns.load_dataset('titanic')
-#titanic['deck'] = titanic['deck'].cat.add_categories('Unknown')
-#titanic['deck'].fillna('Unknown', inplace=True)
-#print(titanic['deck'])
-#print(titanic.info())
+# print(titanic['sex'].head())
+# print(titanic.info())
+gender_survived = titanic.groupby(by='sex')['survived'].mean()
+print(gender_survived)
 
-#print(titanic['survived'])
-#print(titanic.info())
 
 # 생존자 수와 사망자 수 시각화
-sns.countplot(data=titanic, x='survived')
-plt.title('Survived (0 = No, 1 = Yes)')
-plt.xlabel('Survived')
-plt.ylabel('Count')
-plt.show()
+# sns.countplot(data=titanic, x='survived')
+# plt.title('Survived (0 = No, 1 = Yes)')
+# plt.xlabel('Survived')
+# plt.ylabel('Count')
+# plt.show()
